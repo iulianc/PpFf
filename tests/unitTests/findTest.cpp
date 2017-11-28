d0e737bd41739a397510e2c2897d89debeb8b15c
Guy Tremblay <tremblay.guy@uqam.ca>
2017-11-28 11:09:04 -0500

    Nettoie/simplifie findTest et ajoute cas avec grand nombre d'elements

--- tests/unitTests/findTest.cpp
+++ tests/unitTests/findTest.cpp
@@ -6,87 +6,107 @@
 #include "utility.hpp"
 
 bool FuncFind(int in){
-	if(in % 2 == 0){
-		return true;
-	}
-
-	return false;
+    return in % 2 == 0;
 };
 
 
 TEST_CASE("FindEvenElementsUsingFunction", "FindOperator") {
-	std::vector< int > elems(10);
-	std::vector< int > expectedResult(10);
-	expectedResult = {0,2,4,6,8};
-	std::vector< int > currentResult;
+    std::vector<int> elems(10);
+    std::vector<int> expectedResult(10);
+    expectedResult = {0, 2, 4, 6, 8};
+    std::vector<int> currentResult;
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		elems[i] = i;
-	};
+    for(unsigned int i = 0; i < elems.size(); i++) {
+        elems[i] = i;
+    };
 
-	//typedef bool (*findF)(int);
+    //typedef bool (*findF)(int);
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< int >(elems.begin(), elems.end())
-					.find< int >(FuncFind)
-					.collect< int, std::vector >();
+    pp::Pipe pipe;
+    currentResult = pipe
+        .source<int>(elems.begin(), elems.end())
+        .find<int>(FuncFind)
+        .collect<int, std::vector>();
 
-	for(unsigned int i = 0; i < expectedResult.size(); i++){
-		REQUIRE(expectedResult[i] == currentResult[i]);
-	};
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
 }
 
 TEST_CASE("FindOddElementsUsingLambdaFunction", "FindOperator") {
-	std::vector< int > elems(10);
-	std::vector< int > expectedResult(10);
-	expectedResult = {1,3,5,7,9};
-	std::vector< int > currentResult;
+    std::vector<int> elems(10);
+    std::vector<int> expectedResult(10);
+    expectedResult = {1, 3, 5, 7, 9};
+    std::vector<int> currentResult;
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		elems[i] = i;
-	};
+    for (unsigned int i = 0; i < elems.size(); i++) {
+        elems[i] = i;
+    };
 
-	//typedef bool (*findF)(int);
+    //typedef bool (*findF)(int);
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< int >(elems.begin(), elems.end())
-					.find< int >(([](int in)->bool {if(in % 2 != 0) return true; return false;}))
-					.collect< int, std::vector >();
+    pp::Pipe pipe;
+    currentResult = pipe
+        .source<int>(elems.begin(), elems.end())
+        .find<int>( [](int in) ->bool { return in % 2 != 0; } )
+        .collect<int, std::vector>();
 
-	for(unsigned int i = 0; i < expectedResult.size(); i++){
-		REQUIRE(expectedResult[i] == currentResult[i]);
-	};
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
+}
+
+
+TEST_CASE("FindOddElementsUsingLambdaFunction with large number of elements", "FindOperator") {
+    int n = 100000;
+    std::vector<int> elems(n);
+    std::vector<int> expectedResult(n/2);
+    std::vector<int> currentResult;
+
+    for (unsigned int i = 0; i < elems.size(); i++) {
+        elems[i] = i;
+        if (i % 2 == 0) {
+            expectedResult[i/2] = i+1;
+        }
+    };
+
+    //typedef bool (*findF)(int);
+
+    pp::Pipe pipe;
+    currentResult = pipe
+        .source<int>(elems.begin(), elems.end())
+        .parallel(3)
+        .find<int>( [](int in) ->bool { return in % 2 != 0; } )
+        .collect<int, std::vector>();
+
+    std::sort(currentResult.begin(), currentResult.end());
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
 }
 
 
 TEST_CASE("FilterEmployeeWithSalaryBiggerThanHundred", "FindOperator") {
-	std::vector< Employee > elems;
-	std::vector< std::string > expectedResult(3);
-	expectedResult = {"Employee3","Employee6","Employee9"};
-	std::vector< std::string > currentResult;
-	unsigned int noEmployees = 10;
+    std::vector<Employee> elems;
+    std::vector<std::string> expectedResult(3);
+    expectedResult = {"Employee3","Employee6","Employee9"};
+    std::vector<std::string> currentResult;
+    unsigned int noEmployees = 10;
 
-	for(unsigned int i = 0; i < noEmployees; i++){
-		Employee employee;
-		employee.age = i * 10;
-		employee.salary = i%3 == 0 ? i * 100 : i * 10;
-		employee.name = "Employee" + ConvertNumberToString(i);
+    for (unsigned int i = 0; i < noEmployees; i++) {
+        Employee employee;
+        employee.age = i * 10;
+        employee.salary = i % 3 == 0 ? i * 100 : i * 10;
+        employee.name = "Employee" + ConvertNumberToString(i);
 
-		elems.push_back(employee);
-	};
+        elems.push_back(employee);
+    };
 
-	//typedef bool (*retrieveEmployeesWithSalaryBiggerThanHundred)(Employee);
-	//typedef std::string (*retrieveNameEmployee)(Employee);
+    //typedef bool (*retrieveEmployeesWithSalaryBiggerThanHundred)(Employee);
+    //typedef std::string (*retrieveNameEmployee)(Employee);
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee >(elems.begin(), elems.end())
-					.find< Employee >(([](Employee e)->bool {if(e.salary > 100) return true; return false;}))
-					.map< Employee, std::string >(([](Employee e)->std::string {return e.name;}))
-					.collect< std::string, std::vector >();
+    pp::Pipe pipe;
+    currentResult = pipe
+        .source<Employee>(elems.begin(), elems.end())
+        .find<Employee>( [](Employee e) ->bool { return e.salary > 100; } )
+        .map<Employee, std::string>( [](Employee e) ->std::string { return e.name; } )
+        .collect<std::string, std::vector>();
 
-	for(unsigned int i = 0; i < expectedResult.size(); i++){
-		REQUIRE(expectedResult[i] == currentResult[i]);
-	};
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
 }
 
 
