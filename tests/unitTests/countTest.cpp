45f69b5fc76f420c88542c71ca4c3828b3e4e549
Guy Tremblay <tremblay.guy@uqam.ca>
2017-11-28 11:29:00 -0500

    Nettoie/simplifie countTest et definit cas avec grand nombre d'elements

--- tests/unitTests/countTest.cpp
+++ tests/unitTests/countTest.cpp
@@ -4,106 +4,111 @@
 #include "utility.hpp"
 
 TEST_CASE("CollectionOfIntegers", "CountOperator") {
-	std::vector< int > elems(10);
-	unsigned int expectedResult = 10;
-	unsigned int currentResult = 0;
+    int n = 10000;
+    std::vector<int> elems(n);
+    unsigned int expectedResult = n;
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		elems[i] = i;
-	};
+    for (unsigned int i = 0; i < elems.size(); i++) {
+        elems[i] = i;
+    };
 
-	pp::Pipe pipe;
-	currentResult = pipe.source<int>(elems.begin(), elems.end())
-					.count();
+    pp::Pipe pipe;
+    unsigned int currentResult = pipe
+        .source<int>(elems.begin(), elems.end())
+        .parallel(4)
+        .count();
 
-	REQUIRE(expectedResult == currentResult);
+    REQUIRE(expectedResult == currentResult);
 }
 
 
 TEST_CASE("CollectionOfObjects", "CountOperator") {
-	std::vector< Employee > elems;
-	unsigned int expectedResult = 15;
-	unsigned int currentResult = 0;
+    int n = 15;
+    std::vector<Employee> elems;
+    unsigned int expectedResult = n;
 
-	for(unsigned int i = 0; i < expectedResult; i++){
-		Employee employee;
-		employee.age = i + 1;
-		employee.name = "Employee" + ConvertNumberToString(i);
-		employee.salary = 25000;
+    for (unsigned int i = 0; i < n; i++) {
+        Employee employee;
+        employee.age = i + 1;
+        employee.name = "Employee" + ConvertNumberToString(i);
+        employee.salary = 25000;
 
-		elems.push_back(employee);
-	};
+        elems.push_back(employee);
+    };
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee >(elems.begin(), elems.end())
-					.count();
+    pp::Pipe pipe;
+    unsigned int currentResult = pipe
+        .source<Employee>(elems.begin(), elems.end())
+        .count();
 
-	REQUIRE(expectedResult == currentResult);
+    REQUIRE(expectedResult == currentResult);
 }
 
 TEST_CASE("CollectionOfPointerObjects", "CountOperator") {
-	std::vector< Employee* > elems;
-	unsigned int expectedResult = 15;
-	unsigned int currentResult = 0;
+    std::vector<Employee*> elems;
+    unsigned int expectedResult = 15;
 
-	for(unsigned int i = 0; i < expectedResult; i++){
-		Employee *employee = new Employee();
-		employee->age = i + 1;
-		employee->name = "Employee" + ConvertNumberToString(i);
-		employee->salary = 25000;
+    for (unsigned int i = 0; i < expectedResult; i++) {
+        Employee *employee = new Employee();
+        employee->age = i + 1;
+        employee->name = "Employee" + ConvertNumberToString(i);
+        employee->salary = 25000;
 
-		elems.push_back(employee);
-	};
+        elems.push_back(employee);
+    };
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee* >(elems.begin(), elems.end())
-					.count();
+    pp::Pipe pipe;
+    unsigned int currentResult = pipe
+        .source<Employee*>(elems.begin(), elems.end())
+        .count();
 
-	REQUIRE(expectedResult == currentResult);
+    REQUIRE(expectedResult == currentResult);
 }
 
 
 TEST_CASE("CountOnCollectionTypeDeque", "CountOperator") {
-	std::deque< Employee > elems;
-	unsigned int expectedResult = 25;
-	unsigned int currentResult = 0;
+    int n = 2500;
+    std::deque<Employee> elems;
+    unsigned int expectedResult = n;
 
-	for(unsigned int i = 0; i < 25; i++){
-		Employee employee;
-		employee.age = i + 1;
-		employee.name = "Employee" + ConvertNumberToString(i);
-		employee.salary = 130000;
+    for (unsigned int i = 0; i < n; i++) {
+        Employee employee;
+        employee.age = i + 1;
+        employee.name = "Employee" + ConvertNumberToString(i);
+        employee.salary = 130000;
 
-		elems.push_back(employee);
-	};
+        elems.push_back(employee);
+    };
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee >(elems.begin(), elems.end())
-					.count();
+    pp::Pipe pipe;
+    unsigned int currentResult = pipe
+        .source<Employee>(elems.begin(), elems.end())
+        .count();
 
-	REQUIRE(expectedResult == currentResult);
+    REQUIRE(expectedResult == currentResult);
 }
 
 
 TEST_CASE("CountParallel", "CountOperator") {
-	std::vector< Employee > elems(15);
-	unsigned int expectedResult = 15;
-	unsigned int currentResult = 0;
+    int n = 1500;
+    std::vector<Employee> elems(n);
+    unsigned int expectedResult = n;
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		Employee employee;
-		employee.age = i + 1;
-		employee.name = "Employee" + ConvertNumberToString(i);
-		employee.salary = 45000;
+    for (unsigned int i = 0; i < elems.size(); i++) {
+        Employee employee;
+        employee.age = i + 1;
+        employee.name = "Employee" + ConvertNumberToString(i);
+        employee.salary = 45000;
 
-		elems[i] = employee;
-	};
+        elems[i] = employee;
+    };
 
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee >(elems.begin(), elems.end())
-					.parallel(4)
-					.count();
+    pp::Pipe pipe;
+    unsigned int currentResult = pipe
+        .source<Employee>(elems.begin(), elems.end())
+        .parallel(4)
+        .count();
 
-	REQUIRE(expectedResult == currentResult);
+    REQUIRE(expectedResult == currentResult);
 }
 
