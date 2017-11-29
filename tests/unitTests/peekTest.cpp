159f650b8de6e51b8a03ef9d3c1b57a0510274f9
Guy Tremblay <tremblay.guy@uqam.ca>
2017-11-29 07:00:17 -0500

    Nettoie/simplifie peekTest

--- tests/unitTests/peekTest.cpp
+++ tests/unitTests/peekTest.cpp
@@ -5,56 +5,53 @@
 #include "utility.hpp"
 
 TEST_CASE("PrintElementsCollection", "PeekOperator") {
-	std::vector< int > elems(10);
-	std::vector< int > expectedResult(10);
-	expectedResult = {0,1,2,3,4,5,6,7,8,9};
-	std::vector< int > currentResult;
+    std::vector<int> elems(10);
+    std::vector<int> expectedResult(10);
+    
+    for(unsigned int i = 0; i < elems.size(); i++){
+        elems[i] = i;
+        expectedResult[i] = i;
+    };
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		elems[i] = i;
-	};
+    //typedef void (*printElemetsCollection)(int);
 
-	//typedef void (*printElemetsCollection)(int);
+    std::cout << "Elements of collection: ";
+    pp::Pipe pipe;
+    std::vector<int> currentResult = pipe
+        .source<int>(elems.begin(), elems.end())
+        .peek<int>( [](int in) { std::cout << in << "; "; } )
+        .collect< int, std::vector >();
+    std::cout << "\n";
 
-	std::cout << "Elements of collection: ";
-	pp::Pipe pipe;
-	currentResult = pipe.source< int >(elems.begin(), elems.end())
-					.peek< int >(([](int in) {std::cout << in << "; ";}))
-					.collect< int, std::vector >();
-	std::cout << "\n";
-
-	for(unsigned int i = 0; i < expectedResult.size(); i++){
-		REQUIRE(expectedResult[i] == currentResult[i]);
-	};
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
 }
 
 
 TEST_CASE("PrintPropertyObject", "PeekOperator") {
-	std::vector< Employee > elems(10);
-	std::vector< std::string > expectedResult(10);
-	expectedResult = {"Employee0","Employee1","Employee2","Employee3","Employee4","Employee5","Employee6","Employee7","Employee8","Employee9"};
-	std::vector< Employee > currentResult;
+    std::vector<Employee> elems(10);
+    std::vector< std::string > expectedResult(10);
+    expectedResult = {"Employee0","Employee1","Employee2","Employee3","Employee4","Employee5","Employee6","Employee7","Employee8","Employee9"};
 
 
-	for(unsigned int i = 0; i < elems.size(); i++){
-		Employee employee;
-		employee.age = i * 10;
-		employee.salary = i%3 == 0 ? i * 100 : i * 10;
-		employee.name = "Employee" + ConvertNumberToString(i);
-		elems[i] = employee;
-	};
+    for(unsigned int i = 0; i < elems.size(); i++){
+        Employee employee;
+        employee.age = i * 10;
+        employee.salary = i%3 == 0 ? i * 100 : i * 10;
+        employee.name = "Employee" + ConvertNumberToString(i);
+        elems[i] = employee;
+    };
 
-	//typedef void (*printNameEmployee)(Employee);
+    //typedef void (*printNameEmployee)(Employee);
 
-	std::cout << "Name of Employees: ";
-	pp::Pipe pipe;
-	currentResult = pipe.source< Employee >(elems.begin(), elems.end())
-					.peek< Employee >(([](Employee e) {std::cout << e.name << "; ";}))
-					.collect< Employee, std::vector >();
-	std::cout << "\n";
+    std::cout << "Name of Employees: ";
+    pp::Pipe pipe;
+    std::vector<Employee> currentResult = pipe
+        .source<Employee>(elems.begin(), elems.end())
+        .peek<Employee>( [](Employee e) { std::cout << e.name << "; "; } )
+        .collect< Employee, std::vector >();
+    std::cout << "\n";
 
-	for(unsigned int i = 0; i < expectedResult.size(); i++){
-		REQUIRE(expectedResult[i] == ((Employee)currentResult[i]).name);
-	};
-
+    for(unsigned int i = 0; i < expectedResult.size(); i++){
+        REQUIRE(expectedResult[i] == ((Employee)currentResult[i]).name);
+    };
 }
