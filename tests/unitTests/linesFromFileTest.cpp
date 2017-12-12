3dbc74c4f2ca1cd9b207ede5a0c8dbd0ca122784
Guy Tremblay <tremblay.guy@uqam.ca>
2017-12-12 15:06:08 -0500

    Ajoute cas de tests pour linesFromFile

--- tests/unitTests/linesFromFileTest.cpp
+++ tests/unitTests/linesFromFileTest.cpp
@@ -11,7 +11,6 @@
 #define TEST_FILE "/home/iuly/WorkplaceEclipse/PpFf/tests/unitTests/testdata/lorem.txt"
 #endif
 
-
 TEST_CASE( "GetDataFromFileIntoCollectionUsingLines", "LinesFromFileOperator" ) {
     std::vector<std::string> expectedResult = {
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
@@ -20,7 +19,26 @@ TEST_CASE( "GetDataFromFileIntoCollectionUsingLines", "LinesFromFileOperator" ) 
     };
     
     std::string test_file = TEST_FILE;
-    typedef std::vector<std::string> vec_type;
+
+    pp::Pipe pipe;
+    std::vector<std::string> currentResult = pipe
+        .linesFromFile(test_file)
+        .collect<std::string, std::vector>();
+
+    REQUIRE_THAT( currentResult, Catch::Equals(expectedResult) );
+}
+
+TEST_CASE( "GetDataFromFileIntoCollectionUsingLinesAvecLignesBlanches", "LinesFromFileOperator" ) {
+    std::vector<std::string> expectedResult = {
+        "abc def", "",
+        "   ghi j", "",
+        "k", "  ",
+        "l", "",
+        "m n o", "",
+        "p", "", ""
+    };
+    
+    std::string test_file = "testdata/avec-lignes-blanches.txt";
 
     pp::Pipe pipe;
     std::vector<std::string> currentResult = pipe
