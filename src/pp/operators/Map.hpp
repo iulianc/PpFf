355b8bf6cf06ee4218b70b8653d1f8e1bb9f4332
Guy Tremblay <tremblay.guy@uqam.ca>
2017-12-19 09:23:03 -0500

    Ajoute times a Map

--- src/pp/operators/Map.hpp
+++ src/pp/operators/Map.hpp
@@ -7,13 +7,27 @@ using namespace ff;
 template < typename In, typename Out >
 class Map: public ff_node {
 public:
-	Map(std::function< Out*(In*) > const& taskFunc): taskFunc(taskFunc){};
-	~Map(){};
+    Map(std::function< Out*(In*) > const& taskFunc): taskFunc(taskFunc){};
+    ~Map(){};
+    
+    int svc_init() {
+        myTimer = startTimer();
+        
+        return 0;
+    }
+    
+    void* svc(void* task) {
+        return taskFunc((In*)task);
+    }
 
-	void* svc(void* task) {
-		return taskFunc((In*)task);
-	}
+    void svc_end() {
+        long duration_ms = stopTimer(myTimer);
+        
+        if (displayTimer())
+            std::cerr << "Map[" << myTimer << "]: " << duration_ms << " ms" << std::endl;
+    }
 
 private:
-	std::function< Out*(In*) > const& taskFunc;
+    std::function< Out*(In*) > const& taskFunc;
+    int myTimer;
 };
