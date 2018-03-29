2204742422eafd380190c1174a20186e1eef47e5
Guy Tremblay <tremblay.guy@uqam.ca>
2018-03-29 10:38:54 -0400

    Met en forme et change un nom de methode et la rend privee

--- Release2/PpFf/src/stages/Collectors.hpp
+++ Release2/PpFf/src/stages/Collectors.hpp
@@ -8,7 +8,13 @@ namespace PpFf {
     public:
         typedef typename TOperator::Value Value;
 
-        void reduce() {
+        Value value() {
+            combineWorkersResults();
+            return this->workers[0]->value();
+        }
+
+    private:
+        void combineWorkersResults() {
             if (this->workers.size() > 0) {
                 for (unsigned int i = 1; i < this->workers.size(); i++) {
                     *(this->workers[0]) += *(this->workers[i]);
@@ -16,10 +22,6 @@ namespace PpFf {
             }
         }
         
-        Value value(){
-            reduce();
-            return this->workers[0]->value();
-        }
     };
 
 }
