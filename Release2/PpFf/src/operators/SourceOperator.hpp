dc95b62b77de4d1d5aadc5a8ef58499b0e64f47b
Guy Tremblay <tremblay.guy@uqam.ca>
2018-04-06 15:47:40 -0400

    Met en forme le code pour SourceOperator

--- Release2/PpFf/src/operators/SourceOperator.hpp
+++ Release2/PpFf/src/operators/SourceOperator.hpp
@@ -12,18 +12,17 @@ namespace PpFf {
     template< typename T, typename It >
     class SourceOperator: public BaseOperator {
     public:
-        SourceOperator(It const& begin, It const& end): begin(begin), end(end) { }
+        SourceOperator(It const& begin, It const& end): begin(begin), end(end) {}
 
-        SourceOperator(const SourceOperator& other) : begin(other.begin), end(other.end) { }
+        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
 
-        SourceOperator(SourceOperator&& other) noexcept : begin(std::move(other.begin)), end(std::move(other.end)) { }
+        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
 
-        virtual ~SourceOperator() { }
+        virtual ~SourceOperator() {}
 
         void* svc(void* task) {
-            while(begin != end){
+            for (; begin != end; begin++) {
                 this->ff_send_out(new T(*begin));
-                begin ++;
             }
 
             return EOS;
@@ -36,20 +35,19 @@ namespace PpFf {
 
 
     template< typename T, typename It >
-    class SourceOperator< T*, It >: public BaseOperator {
+    class SourceOperator<T*, It>: public BaseOperator {
     public:
         SourceOperator(It const& begin, It const& end): begin(begin), end(end){}
 
-        SourceOperator(const SourceOperator& other) : begin(other.begin), end(other.end) { }
+        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
 
-        SourceOperator(SourceOperator&& other) noexcept : begin(std::move(other.begin)), end(std::move(other.end)) { }
+        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
 
-        ~SourceOperator(){}
+        ~SourceOperator() {}
 
         void* svc(void* task) {
-            while(begin != end){
+            for (; begin != end; begin++) {
                 this->ff_send_out(&(*begin));
-                begin ++;
             }
 
             return EOS;
