e5b75c0a8e4070eabe84f0b45bc36c48ff56ae51
Guy Tremblay <tremblay.guy@uqam.ca>
2018-04-13 10:50:57 -0400

    Ajoute classe + methode pour deboguer

--- Release2/PpFf/src/Pipe.hpp
+++ Release2/PpFf/src/Pipe.hpp
@@ -24,6 +24,7 @@
 #include <stages/Stage.hpp>
 #include <stages/Collectors.hpp>
 #include <stages/BaseStage.hpp>
+#include <utilities/Debug.hpp>
 #include <vector>
 #include <map>
 #include <unordered_map>
