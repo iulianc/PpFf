1a738014f29e59e52518fa3f6cf44c2bae391e7c
Guy Tremblay <tremblay.guy@uqam.ca>
2019-07-19 15:42:28 -0400

    Modifie pour gnuplot et pour avoir des fichiers plus courts pour deboguer le script

--- Release2/PpFf/tests/WordCount/run_bms.rb
+++ Release2/PpFf/tests/WordCount/run_bms.rb
@@ -1,18 +1,27 @@
 #!/usr/bin/env ruby
 
+DEBUG = true
+
 LARGEUR = 8
 
-NB_REPETITIONS = 5
+if DEBUG
+  NB_REPETITIONS = 1 # Pour debogage: sinon 5!
+else
+  NB_REPETITIONS = 5
+end
 
-NB_MOTS =
-  [78792, 167941, 281307, 482636, 752856, 2614743, 5247678]
-# [78792, 167941] # Pour debogage du script.
-# [497,78792,5247678] # Pour debogage du script.
+if DEBUG
+  NB_MOTS = [10, 131, 377, 3805, 7610] # Pour debogage du script.
+else
+  NB_MOTS = [78792, 167941, 281307, 482636, 752856, 2614743, 5247678]
+end
 
-# Sur japet, plante si plus que 64, donc au plus 32... fois 2 -- voir plus bas.
-MAX_NB_THREADS =
-  # [`cat /proc/cpuinfo | grep processor | wc -l`.to_i, 32].min
-  2 # Pour debogage du script
+if DEBUG
+  NB_THREADS = [1, 2]
+else
+  # Sur japet, plante si plus que 64..
+  NB_THREADS = [1, 2, 4] # ????
+end
 
 
 def temps_moyen( cmd )
@@ -26,26 +35,27 @@ def temps_moyen( cmd )
   (temps_tot / NB_REPETITIONS).to_i
 end
 
+print format("\#%#{LARGEUR}s %#{LARGEUR}s %#{LARGEUR}s",
+            "N", "Java+", "Java-")
+
+NB_THREADS.each do |nb_threads|
+  print format(" %#{LARGEUR}s", "PpFf#{nb_threads}" )
+end
+print "\n"
+
 NB_MOTS.each do  |nb_mots|
+  print " #{format("%#{LARGEUR}d", nb_mots)}"
+
   fichier_mots = "testdata/#{nb_mots}Words.txt"
-
-  puts "# Nb. mots = #{nb_mots}"
-  puts format("\#%#{LARGEUR-1}s %#{LARGEUR}s %#{LARGEUR}s %#{LARGEUR}s",
-              "Nbt", "Java+", "Java-", "PpFf")
-
 
   temps_java_avec_jit = temps_moyen "java -cp . WordCount '#{fichier_mots}' 4 #{nb_mots} 0"
   temps_java_sans_jit = temps_moyen "java -Djava.compiler=NONE -cp . WordCount '#{fichier_mots}' 4 #{nb_mots} 0"
 
-  nb_threads = 1
-  while nb_threads <= MAX_NB_THREADS do
-    print "#{format("%#{LARGEUR}d", nb_threads)}"
-    print " #{format("%#{LARGEUR}d", temps_java_avec_jit)}"
-    print " #{format("%#{LARGEUR}d", temps_java_sans_jit)}"
-
+  print " #{format("%#{LARGEUR}d", temps_java_avec_jit)}"
+  print " #{format("%#{LARGEUR}d", temps_java_sans_jit)}"
+  NB_THREADS.each do |nb_threads|
     temps_ppff = temps_moyen "./WordCount #{fichier_mots} 4 #{nb_threads}"
     print " #{format("%#{LARGEUR}d", temps_ppff)}"
-    print "\n"
-    nb_threads *= 2
   end
+  print "\n"
 end
