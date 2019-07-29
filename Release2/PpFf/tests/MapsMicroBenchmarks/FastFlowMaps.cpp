59f18e1c79dff7b96ddf1f2b6836d949b6d2f5c7
iulianc <iulianc_hd@yahoo.com>
2019-07-28 21:48:33 -0400

    Maps - Micro benchmarks

---
+++ Release2/PpFf/tests/MapsMicroBenchmarks/FastFlowMaps.cpp
@@ -0,0 +1,224 @@
+#include <iostream>
+#include <ff/ff.hpp>
+#include <functional>
+#include <regex>
+#include <chrono>
+#include <ctime>
+#include <ratio>
+#include <vector>
+#include <map>
+#include <unordered_map>
+#include <string>
+#include <sstream>
+#include<fstream>
+#include <locale>
+#include <unordered_map>
+#include <ctype.h>
+#include <cassert>
+#include <cmath>
+
+
+using namespace ff;
+
+static unsigned granularite = 2;
+
+class Worker1: public ff_node {
+public:
+    void* svc(void* task) {
+		 printf("Worker1(%ld): TASK COMPUTED\n", get_my_id());
+       return task;
+    }
+};
+
+class Emitter1: public ff_node {
+public:
+    Emitter1(int ntasks):ntasks(ntasks) {}
+    void* svc(void*) {
+        for(long i=0;i<ntasks;++i)
+            ff_send_out((void*)(i+10));
+        return NULL;
+    }
+private:
+    int ntasks;
+};
+
+
+struct Source: ff_node {
+	std::vector<int> elems;
+
+	Source(std::vector<int> elems): elems(elems){}
+
+    void* svc(void* task) {
+    	for (unsigned int i = 0; i < elems.size(); i++) {
+			ff_send_out(new int(elems[i]));
+		}
+
+    	return EOS;
+    }
+};
+
+static int* mapFunc(int *in){
+    int* res = (int*) malloc(sizeof(int));
+    *res = *in;
+    int nb = pow(10, granularite);
+    for (int i = 1; i <= nb; i++) {
+        *res += 1;
+    }
+    return res;
+};
+
+struct Map: ff_node {
+
+    void* svc(void* task) {
+    	int* in = (int*)task;
+    	return mapFunc(in);
+    }
+};
+
+struct Collect: ff_node {
+	std::vector<int> &result;
+
+	Collect(std::vector<int> &result): result(result){}
+
+    void* svc(void* task) {
+    	int* in = (int*)task;
+    	result.push_back(*in);
+
+		return GO_ON;
+    }
+};
+
+struct Empty: public ff_node {
+	public:
+   	void* svc(void* task) {
+      	return task;
+      }
+};
+
+int main(int argc, char* argv[]) {
+	
+   // On definit les parametres d'execution.
+   unsigned int n = 1000;
+   unsigned int nbMaps = 2;
+   unsigned int nbThreads = 1; // Execution sequentielle par defaut.
+    
+   int numArg = 1;
+   if (argc > numArg) {
+       n = atoi(argv[numArg]);
+       assert( n > 0 );
+   }
+   numArg += 1;
+   if (argc > numArg) {
+       nbMaps = atoi(argv[numArg]);
+       assert( nbMaps >= 1 );
+   }
+   numArg += 1;
+   if (argc > numArg) {
+       granularite = atoi(argv[numArg]);
+       assert( granularite >= 0 );
+   }
+   numArg += 1;
+   if (argc > numArg) {
+       nbThreads = atoi(argv[numArg]);
+       assert( nbThreads > 0 );
+   }
+   numArg += 1;
+
+   // On definit les elements a traiter et le resultat attendu.
+   std::vector<int> elems(n);
+   std::vector<int> attendus(n);
+   for (unsigned int i = 0; i < n; i++) {
+       elems[i] = i;
+       attendus[i] = nbMaps * pow(10, granularite) + i;
+   }
+    
+   std::vector<int> obtenus;
+   
+   auto begin = std::chrono::high_resolution_clock::now();
+
+	ff_pipeline ffp;
+	ff_farm farm;
+	std::vector<ff_node*> workers;
+
+	if(nbThreads > 1){
+		ffp.add_stage(new Source(elems));
+		//farm.add_emitter(new Source(elems));
+		
+		for(uint32_t i = 0; i < nbThreads; i++){
+			workers.push_back(new Map());
+		}		
+
+		farm.add_workers(workers);
+		
+		//farm.add_collector(NULL);
+		//farm.remove_collector();
+		
+
+		//farm.add_collector(new Collect(obtenus));
+		farm.add_collector(new Empty());
+
+		ffp.add_stage(&farm);
+		ffp.add_stage(new Collect(obtenus));
+
+	}else {
+		ffp.add_stage(new Source(elems));
+		ffp.add_stage(new Map());
+		ffp.add_stage(new Collect(obtenus));
+	}
+	
+	if (ffp.run_and_wait_end()<0) error("running pipe");
+	
+
+    auto end = std::chrono::high_resolution_clock::now();
+ 
+   
+    std::sort(obtenus.begin(), obtenus.end());
+
+
+    if (obtenus.size() != attendus.size()) {
+        fprintf( stderr, 
+                 "Tailles pas ok: obtenus = %lu vs. attendus = %lu\n", 
+                 obtenus.size(), attendus.size() );
+    }
+ 
+    for (unsigned int i = 0; i < attendus.size(); i++) {
+        if (obtenus[i] != attendus[i]) {
+            fprintf( stderr, 
+                     "Pas ok pour %d: obtenus = %d vs. attendus = %d\n", 
+                     i, obtenus[i], attendus[i] );
+            break;
+        }
+    }
+  
+
+    long duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
+    printf( "%d %d %d %d : %ld\n", n, nbMaps, granularite, nbThreads, duration_ms );
+ 
+
+   return 0;
+}
+
+
+
+
+/*
+
+        // EXECUTION PARALLELE.
+    // On construit le pipe.
+
+        auto pipe = 
+            Flow
+            ::source<int>(elems.begin(), elems.end())
+				.parallel(nbThreads);
+      
+        for (unsigned i = 0; i < nbMaps; i++) {
+            pipe = 
+                pipe
+                .map<int,int>(mapFunc);
+        }
+
+        obtenus = 
+            pipe  
+				.parallel(1)
+            .collect<int, std::vector>();
+*/
---
+++ Release2/PpFf/tests/MapsMicroBenchmarks/JavaMaps.java
@@ -0,0 +1,80 @@
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.Collections;
+
+
+public class JavaMaps {
+    static int n = 10;
+    static int nbMaps = 1;
+    static int granularite = 1;  
+    static int nbThreads = 1;  
+    
+    static int mapFunc(int in){
+        int res = in;
+        int nb = (int)Math.pow(10, granularite);
+        for (int i = 1; i <= nb; i++) {
+            res += 1;
+        }
+        return res;
+    };
+    
+    public static void main(String[] args) {
+       
+        if (args.length >= 1) {
+            n = Integer.parseInt(args[0]);
+    	}
+
+    	if (args.length >= 2) {
+            nbMaps = Integer.parseInt(args[1]);
+    	}
+
+    	if (args.length >= 3) {
+            granularite = Integer.parseInt(args[2]);
+    	}
+
+    	if (args.length >= 4) {
+            nbThreads = Integer.parseInt(args[3]);
+    	}
+        
+        ArrayList<Integer> elems = new ArrayList<Integer>();
+        ArrayList<Integer> attendus = new ArrayList<Integer>();
+        List<Integer> obtenus = null;
+        
+        for(int i = 0; i < n; i++) {
+            elems.add(i);
+            attendus.add((int)(nbMaps * Math.pow(10, granularite) + i));            
+        }
+        
+        
+        long startTime = System.nanoTime();
+        
+        obtenus = elems.stream()
+                .parallel()
+                .map(JavaMaps::mapFunc)
+                .collect(Collectors.toList());
+                         
+
+        long duration = (System.nanoTime() - startTime);
+        double milliseconds = (double) duration / 1000000;
+        
+        if (obtenus.size() != attendus.size()) {
+            System.out.println(String.format("Tailles pas ok: obtenus = %lu vs. attendus = %lu",
+                    obtenus.size(), attendus.size() ));
+        }        
+        
+        Collections.sort(obtenus);
+             
+        for (int i = 0; i < attendus.size(); i++) {
+            if(!obtenus.get(i).equals(attendus.get(i))) {
+                System.out.println( String.format("Pas ok pour %d: obtenus = %d vs. attendus = %d", i, obtenus.get(i), attendus.get(i)));
+                break;
+            }
+        }        
+        
+        String outputResult = String.format("%5d %2d %2d %2d : %6.0f ", n, nbMaps, granularite, nbThreads, milliseconds);
+        System.out.print(outputResult);         
+    }
+    
+}
