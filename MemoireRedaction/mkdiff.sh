a590676e1b92a2fbd2cf2d10c464548e99f8327f
Guy Tremblay <tremblay.guy@uqam.ca>
2020-04-10 14:42:18 -0400

    Corrige appel a latexdiff: old vient avant new

--- MemoireRedaction/mkdiff.sh
+++ MemoireRedaction/mkdiff.sh
@@ -12,7 +12,7 @@ perl ../LatexDiff/latexpand.pl memoire.tex > flat.tex
 cd ..
 
 echo "latexdiff -c ld.cfg flat.tex VersionAComparer/flat.tex >diff.tex"
-latexdiff -c LatexDiff/ld.cfg flat.tex VersionAComparer/flat.tex >diff.tex
+latexdiff -c LatexDiff/ld.cfg VersionAComparer/flat.tex flat.tex >diff.tex
 
 pdflatex diff
 
