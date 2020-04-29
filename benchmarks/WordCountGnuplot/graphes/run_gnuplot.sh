860480293fe4e49c3e3f7184ed1c2305b2ca735c
Guy Tremblay <tremblay.guy@uqam.ca>
2020-04-29 11:08:02 -0400

    Adapte pour que cela fonctionne sur MacOS

--- benchmarks/WordCountGnuplot/graphes/run_gnuplot.sh
+++ benchmarks/WordCountGnuplot/graphes/run_gnuplot.sh
@@ -101,6 +101,32 @@ elif [ $server == 'c34581' ]; then
 	 "$fichier" using 1:6 title "PpFf-4" with linespoints
 EOF
 
+elif [ $server == 'MacOS' ]; then
+    if [[ $DEBUG ]]; then
+        echo "*** Traitement pour $server ***"
+        echo "taille_min = $taille_min; taille_max = $taille_max"
+        echo "temps_min = $temps_min; temps_max = $temps_max"
+    fi
+    gnuplot -persist <<EOF
+    set terminal png
+    set output '$output_graph'
+    set logscale y
+    set format x '%.0f'
+    set xtics rotate by 310
+    set xtics font ", 6"
+    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
+    #
+    set xlabel "Nombre de mots traités"
+    set ylabel "Temps d'exécution (log ms)"
+    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
+
+    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
+	 "$fichier" using 1:2 title "Java+" with linespoints,\
+	 "$fichier" using 1:3 title "Java-" with linespoints,\
+	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
+	 "$fichier" using 1:5 title "PpFf-2" with linespoints
+EOF
+
 else
     gnuplot -persist <<EOF
     set terminal png
