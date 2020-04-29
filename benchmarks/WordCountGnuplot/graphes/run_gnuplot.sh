18a453092436c5760d84fcbeb54ae7dd7d42556a
Guy Tremblay <tremblay.guy@uqam.ca>
2020-04-29 13:22:28 -0400

    Poursuit nettoyage du script pour les graphes

--- benchmarks/WordCountGnuplot/graphes/run_gnuplot.sh
+++ benchmarks/WordCountGnuplot/graphes/run_gnuplot.sh
@@ -29,129 +29,54 @@ temps_max=$(ruby max_temps.rb <$fichier)
 taille_min=0 # 0 plus simple pcq. non log
 taille_max=$(ruby max_taille.rb <$fichier)
 
-if [ $server == "java" ]; then
-    gnuplot -persist <<EOF
-    set terminal png
-    set output '$output_graph'
-    set logscale y
-    set format x '%.0f'
-    set xtics rotate by 310
-    set xtics font ", 6"
-    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
-    #
-    set xlabel "Nombre de mots traités"
-    set ylabel "Temps d'exécution (log ms)"
-    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
+if [[ $DEBUG ]]; then
+    echo "*** Traitement pour $server ***"
+    echo "taille_min = $taille_min; taille_max = $taille_max"
+    echo "temps_min = $temps_min; temps_max = $temps_max"
+fi
 
-    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
-	 "$fichier" using 1:3 title "Java-" with linespoints,\
-	 "$fichier" using 1:5 title "PpFf-2" with linespoints,\
-	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
-	 "$fichier" using 1:2 title "Java+" with linespoints
+cat >script.plot <<EOF
+set terminal png
+set output '$output_graph'
+set logscale y
+set format x '%.0f'
+set xtics rotate by 310
+set xtics font ", 6"
+set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
+
+set xlabel "Nombre de mots traités"
+set ylabel "Temps d'exécution (log ms)"
+set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
 EOF
 
-elif [ $server == 'japet' ]; then
-    gnuplot -persist <<EOF
-    set terminal png
-    set output '$output_graph'
-    set logscale y
-    set format x '%.0f'
-    set xtics rotate by 310
-    set xtics font ", 6"
-    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
-    #
-    set xlabel "Nombre de mots traités"
-    set ylabel "Temps d'exécution (log ms)"
-    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
+/bin/echo -n "plot [$taille_min:$taille_max][$temps_min:$temps_max] " >>script.plot
+/bin/echo -n "\"$fichier\" using 1:2 title 'Java+' with linespoints, " >>script.plot
+/bin/echo -n "\"$fichier\" using 1:3 title 'Java-' with linespoints, " >>script.plot
+/bin/echo -n "\"$fichier\" using 1:4 title 'PpFf-1-' with linespoints, " >>script.plot
+/bin/echo -n "\"$fichier\" using 1:5 title 'PpFf-2' with linespoints" >>script.plot
 
-    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
-	 "$fichier" using 1:2 title "Java+" with linespoints,\
-	 "$fichier" using 1:3 title "Java-" with linespoints,\
-	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
-	 "$fichier" using 1:5 title "PpFf-2" with linespoints,\
-	 "$fichier" using 1:6 title "PpFf-4" with linespoints,\
-	 "$fichier" using 1:7 title "PpFf-8" with linespoints,\
-	 "$fichier" using 1:8 title "PpFf-16" with linespoints
-EOF
 
-elif [ $server == 'c34581' ]; then
-    if [[ $DEBUG ]]; then
-        echo "*** Traitement pour $server ***"
-        echo "taille_min = $taille_min; taille_max = $taille_max"
-        echo "temps_min = $temps_min; temps_max = $temps_max"
-    fi
-    gnuplot -persist <<EOF
-    set terminal png
-    set output '$output_graph'
-    set logscale y
-    set format x '%.0f'
-    set xtics rotate by 310
-    set xtics font ", 6"
-    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
-    #
-    set xlabel "Nombre de mots traités"
-    set ylabel "Temps d'exécution (log ms)"
-    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
+if [[ $server == 'MacOS' || $server == 'java' ]]; then
+    /bin/echo "" >>script.plot
 
-    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
-	 "$fichier" using 1:2 title "Java+" with linespoints,\
-	 "$fichier" using 1:3 title "Java-" with linespoints,\
-	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
-	 "$fichier" using 1:5 title "PpFf-2" with linespoints,\
-	 "$fichier" using 1:6 title "PpFf-4" with linespoints
-EOF
-
-elif [ $server == 'MacOS' ]; then
-    if [[ $DEBUG ]]; then
-        echo "*** Traitement pour $server ***"
-        echo "taille_min = $taille_min; taille_max = $taille_max"
-        echo "temps_min = $temps_min; temps_max = $temps_max"
-    fi
-    gnuplot -persist <<EOF
-    set terminal png
-    set output '$output_graph'
-    set logscale y
-    set format x '%.0f'
-    set xtics rotate by 310
-    set xtics font ", 6"
-    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
-    #
-    set xlabel "Nombre de mots traités"
-    set ylabel "Temps d'exécution (log ms)"
-    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
-
-    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
-	 "$fichier" using 1:2 title "Java+" with linespoints,\
-	 "$fichier" using 1:3 title "Java-" with linespoints,\
-	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
-	 "$fichier" using 1:5 title "PpFf-2" with linespoints
-EOF
+elif [[ $server == 'japet' ]]; then
+    /bin/echo -n ", \"$fichier\" using 1:6 title 'PpFf-4' with linespoints, " >>script.plot
+    /bin/echo -n "\"$fichier\" using 1:7 title 'PpFf-8' with linespoints, " >>script.plot
+    /bin/echo "\"$fichier\" using 1:8 title 'PpFf-16' with linespoints" >>script.plot
 
 else
-    gnuplot -persist <<EOF
-    set terminal png
-    set output '$output_graph'
-    set logscale y
-
-    set format x '%.0f'
-    set xtics rotate by 310
-    set xtics font ", 6"
-    set xtics (78792, 167941, 281307, 482636, 752856, 1639684, 2137758, 2614743)
-    #
-    set xlabel "Nombre de mots traités"
-    set ylabel "Temps d'exécution (log ms)"
-    set title "WordCount: Nombre de mots traités vs. (log) Temps d'exécution\n"
-
-    plot [$taille_min:$taille_max][$temps_min:$temps_max] \
-	 "$fichier" using 1:2 title "Java+" with linespoints,\
-	 "$fichier" using 1:3 title "Java-" with linespoints,\
-	 "$fichier" using 1:4 title "PpFf-1:" with linespoints,\
-	 "$fichier" using 1:5 title "PpFf-2" with linespoints,\
-	 "$fichier" using 1:6 title "PpFf-4" with linespoints,\
-	 "$fichier" using 1:7 title "PpFf-8" with linespoints,\
-	 "$fichier" using 1:8 title "PpFf-16" with linespoints
-EOF
+    /bin/echo ", \"$fichier\" using 1:6 title 'PpFf-4' with linespoints" >>script.plot
 
 fi
 
+if [[ $DEBUG == 1 ]]; then
+    echo "*** Script genere ***"
+    cat script.plot
+fi 
 
+gnuplot -persist <script.plot
+
+if [[ $DEBUG == 1 ]]; then
+    echo "*** Graphe genere! ***"
+    #open $output_graph
+fi
