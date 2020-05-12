e3dd14650c34086b3b2d7ebb764f222095dff589
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-12 14:57:43 -0400

    Redefinit experiences intermediaires sur linux, et pour essai sur MacOS

--- benchmarks/WordCountGnuplot/WordCount-bm-config.rb
+++ benchmarks/WordCountGnuplot/WordCount-bm-config.rb
@@ -10,11 +10,11 @@
 ################################################################
 # Les divers programmmes et commandes pour les executer.
 ################################################################
-Program.define( "Java+", "java -cp . #{PGM} 0" )
-Program.define( "Java-", "java -Djava.compiler=NONE -cp . #{PGM} 0" )
-Program.define( "Java*", "java -cp . #{PGM} 1" )
+Program.define( 'Java+', "java -cp . #{PGM} 0" )
+Program.define( 'Java-', "java -Djava.compiler=NONE -cp . #{PGM} 0" )
+Program.define( 'Java*', "java -cp . #{PGM} 1" )
 
-[1, 2, 4, 8, 16, 32].each do |k|
+[*1..4].each do |k|
   Program.define( "PpFf-#{k}", "./#{PGM} #{k}" )
   Program.define( "FastFlow-#{k}", "./#{PGM}FastFlow #{k}" )
 end
@@ -31,14 +31,15 @@ end
 peu_de_donnees =
   [3805, 7610]
 
-plus_de_donnees =
-  [3805, 7610, 78792]
+donnees_preliminaires =
+  [78792, 281307, 752856, 1639684]
 
 pas_mal_de_donnees =
-  [78792, 167941, 281307, 482636, 752856, 1639684]
+  [78792, 167941, 281307, 482636, 752856]
 
 beaucoup_de_donnees =
-  [1639684, 2614743, 5293812, 10587624]
+  [752856, 1639684, 2614743, 5293812, 10587624]
+
 
 ################################################################
 # Les experiences.
@@ -51,29 +52,45 @@ beaucoup_de_donnees =
 Experience.define( 0,
                    nb_items: peu_de_donnees,
                    nb_repetitions: 2,
-                   programs: ["Java+", "Java-", "Java*",
-                              "PpFf-1", "PpFf-2", "PpFf-4",
-                              "FastFlow-1", "FastFlow-2", "FastFlow-4",
+                   programs: ['Java+', 'Java-', 'Java*',
+                              'PpFf-1', 'PpFf-2', 'PpFf-4',
+                              'FastFlow-1', 'FastFlow-2', 'FastFlow-4',
                              ]
                 )
 
+####################################################
+# Experiences faites sur les machines indiquees pour identifier les
+# programmes de chaque groupe (Java, PpFf et FastFlow) qui sont les
+# plus performants. Ceci permet ensuite d'effectuer la comparaison
+# finale, avec beaucoup de donnees, uniquement avec ces programmes, un
+# par groupe.
+####################################################
 Experience.define( 1,
-                   machines: ['MacOS'],
-                   nb_items: plus_de_donnees,
-                   nb_repetitions: 5,
-                   programs: ["PpFf-1", "PpFf-2", "PpFf-4"]
+                   machines: ['c34581', 'MacOs'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['Java+', 'Java-', 'Java*']
                 )
 
 Experience.define( 2,
-                   machines: ['MacOS'],
-                   nb_items: plus_de_donnees,
-                   nb_repetitions: 5,
-                   programs: ["Java*", "PpFf-2", "FastFlow-1"]
+                   machines: ['c34581', 'MacOS'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['PpFf-1', 'PpFf-2', 'PpFf-3']
                 )
 
 Experience.define( 3,
+                   machines: ['c34581', 'MacOS'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['FastFlow-1', 'FastFlow-2', 'FastFlow-3']
+                )
+
+
+# Les plus performants.
+Experience.define( 4,
                    machines: ['c34581'],
                    nb_items: beaucoup_de_donnees,
                    nb_repetitions: 30,
-                   programs: ["Java*", "PpFf-2", "FastFlow-2"]
+                   programs: ['Java*', 'PpFf-1', 'FastFlow-1']
                 )
