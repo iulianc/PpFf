3c749cfa5bb9b22b2431185d0eeaef3a10d248cf
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-12 16:35:20 -0400

    Modifie la version Java pour qu'elle soit le plus identique possible avec la version PpFf

--- benchmarks/WordCountGnuplot/WordCount.java
+++ benchmarks/WordCountGnuplot/WordCount.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.text.DecimalFormat;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -19,17 +20,53 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.*;
+import java.util.HashMap;
+import java.util.function.*;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
 
 
 /**
  *
- * @author iciobanu
+ * @author iciobanu & Guy Tremblay
  */
+
+class GroupByKey implements Consumer<String> {
+    @SuppressWarnings("unchecked")
+        private Map<String,Integer> map = (Map) new HashMap();
+    
+    public void accept( String s ) {
+        map.put(s, map.getOrDefault(s, 0) + 1);
+    }
+    
+    public void combine( GroupByKey other ) {
+        for ( Map.Entry<String,Integer> entry : other.map.entrySet() ) {
+            Integer value = map.getOrDefault(entry.getKey(), 0);
+            map.put( entry.getKey(), value + entry.getValue() );
+        } 
+    }
+
+    public Map<String,Integer> toMap() {
+        return map;
+    }
+}
+
 public class WordCount {
     static final String DEFAULT_INPUT_FILE = "testdata/78792Words.txt";
   
+    public static Stream<String> splitInWords(String line) {
+        return Arrays.stream(line.trim().split(" "));
+    }
+    
+    public static String toLowerCaseLetters(String word) {
+        return word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+    }
+    
+    public static boolean notEmpty( String word ) {
+        return word.length() > 0;
+    }
+    
     public static void main(String[] args) throws IOException {
         boolean avecWarmup = Integer.parseInt(args[0]) != 0;
         String inputFile = args.length >= 2 ? args[1] : DEFAULT_INPUT_FILE;
@@ -44,8 +81,9 @@ public class WordCount {
             int wordsCount_ = 
                 Files.lines(Paths.get(inputFile))
                 .parallel()
-                .flatMap( line -> Arrays.stream(line.trim().split(" ")) )
-                .filter( word -> word.length() > 0 )
+                .flatMap( WordCount::splitInWords )
+                .map( WordCount::toLowerCaseLetters )
+                .filter( WordCount::notEmpty )
                 .collect( Collectors.toList() )
                 .size();
             System.err.println( wordsCount_ );
@@ -54,16 +92,14 @@ public class WordCount {
         // Execution du *vrai* programme.
         long startTime = System.nanoTime();
         
-        List<Map.Entry<String,Integer>> wordsCount = 
+        Map<String,Integer> wordsCount = 
             Files.lines(Paths.get(inputFile))
             .parallel()
-            .flatMap( line -> Arrays.stream(line.trim().split(" ")) )
-            .map( word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase() )
-            .filter( word -> word.length() > 0 )
-            .map( word -> new SimpleEntry<>(word, 1) )
-            .collect( toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2) )
-            .entrySet().stream()
-            .collect( Collectors.toList() );  
+            .flatMap( WordCount::splitInWords )
+            .map( WordCount::toLowerCaseLetters )
+            .filter( WordCount::notEmpty )
+            .collect( GroupByKey::new, GroupByKey::accept, GroupByKey::combine)
+            .toMap();
         
         long duration = (System.nanoTime() - startTime);
         double milliseconds = (double) duration / 1000000;
@@ -73,7 +109,7 @@ public class WordCount {
             String outputResult = String.format("%6.0f ", milliseconds);
             System.out.print(outputResult); 
         } else {
-            wordsCount.forEach( x -> 
+            wordsCount.entrySet().forEach( x -> 
                                 System.out.println( x.getKey() + " => " + x.getValue() ) );     
         }
     }
