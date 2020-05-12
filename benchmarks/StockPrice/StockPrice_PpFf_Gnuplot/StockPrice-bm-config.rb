3426c037ac57ac0bc6017d56ceda0fe863294ae4
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-12 19:25:46 -0400

    Definit experiences pour StockPrice

--- benchmarks/StockPrice/StockPrice_PpFf_Gnuplot/StockPrice-bm-config.rb
+++ benchmarks/StockPrice/StockPrice_PpFf_Gnuplot/StockPrice-bm-config.rb
@@ -22,14 +22,12 @@ end
 peu_de_donnees =
   [4 * 1024, 8 * 1024]
 
-plus_de_donnees =
-  [8 * 1024, 16 * 1024]
-
-pas_mal_de_donnees =
-  [16, 32, 64].map { |n| n * 1024}
+donnees_preliminaires =
+  [8, 32, 64].map { |n| n * 1024}
 
 beaucoup_de_donnees =
   [16, 32, 64, 128, 256].map { |n| n * 1024 }
+
 
 ################################################################
 # Les experiences.
@@ -42,29 +40,45 @@ beaucoup_de_donnees =
 Experience.define( 0,
                    nb_items: peu_de_donnees,
                    nb_repetitions: 2,
-                   programs: ["Java+", "Java-", "Java*",
-                              "PpFf-1", "PpFf-2", "PpFf-4",
-                              "FastFlow-1", "FastFlow-2", "FastFlow-4",
+                   programs: ['Java+', 'Java-', 'Java*',
+                              'PpFf-1', 'PpFf-2', 'PpFf-4',
+                              'FastFlow-1', 'FastFlow-2', 'FastFlow-4',
                              ]
                 )
 
+####################################################
+# Experiences faites sur les machines indiquees pour identifier les
+# programmes de chaque groupe (Java, PpFf et FastFlow) qui sont les
+# plus performants. Ceci permet ensuite d'effectuer la comparaison
+# finale, avec beaucoup de donnees, uniquement avec ces programmes, un
+# par groupe.
+####################################################
 Experience.define( 1,
-                   machines: ['MacOS'],
-                   nb_items: plus_de_donnees,
-                   nb_repetitions: 5,
-                   programs: ["PpFf-1", "PpFf-2", "PpFf-4"]
+                   machines: ['c34581', 'MacOS'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['Java+', 'Java-', 'Java*']
                 )
 
 Experience.define( 2,
-                   machines: ['MacOS'],
-                   nb_items: plus_de_donnees,
-                   nb_repetitions: 5,
-                   programs: ["Java*", "PpFf-2", "FastFlow-1"]
+                   machines: ['c34581', 'MacOS'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['PpFf-1', 'PpFf-2', 'PpFf-3']
                 )
 
 Experience.define( 3,
+                   machines: ['c34581', 'MacOS'],
+                   nb_items: donnees_preliminaires,
+                   nb_repetitions: 10,
+                   programs: ['FastFlow-1', 'FastFlow-2', 'FastFlow-3']
+                )
+
+
+# Les plus performants.
+Experience.define( 4,
                    machines: ['c34581'],
                    nb_items: beaucoup_de_donnees,
                    nb_repetitions: 30,
-                   programs: ["Java*", "PpFf-2", "FastFlow-2"]
+                   programs: ['Java*', 'PpFf-1', 'FastFlow-1']
                 )
