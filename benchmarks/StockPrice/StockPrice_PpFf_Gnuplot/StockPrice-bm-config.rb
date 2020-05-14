5a68c3d666b87e3454ba6c08f2b5306858387293
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-14 12:43:37 -0400

    Ajoute resultats finaux StockPrice pour java

--- benchmarks/StockPrice/StockPrice_PpFf_Gnuplot/StockPrice-bm-config.rb
+++ benchmarks/StockPrice/StockPrice_PpFf_Gnuplot/StockPrice-bm-config.rb
@@ -85,3 +85,10 @@ Experience.define( 4,
                    nb_repetitions: 30,
                    programs: ['Java*', 'PpFf-2', 'FastFlow-2']
                 )
+
+Experience.define( 7,
+                   machines: ['java'],
+                   nb_items: beaucoup_de_donnees,
+                   nb_repetitions: 30,
+                   programs: ['Java*', 'PpFf-4', 'FastFlow-4']
+                 )
