df39101267b7e59e78681c388ffc137bb2f9e02c
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-15 13:47:02 -0400

    Fait des petites modifications pour rendre le code plus propre et voir si on peut le rendre plus efficace
    Conclusion: pas vraiment, cela semble le mieux qu'on puisse faire avec ces structures de donnees

--- src/operators/GroupByKeyOperator.hpp
+++ src/operators/GroupByKeyOperator.hpp
@@ -14,45 +14,47 @@ namespace PpFf {
         typedef MapContainer Value;
 
         GroupByKeyOperator(std::function< K*(In*) > const& taskFuncOnKey, 
-                           std::function< V*(In*) > const& taskFuncOnValue = identity<In,V>): 
-            taskFuncOnKey(taskFuncOnKey), taskFuncOnValue(taskFuncOnValue) {}
+                           std::function< V*(In*) > const& taskFuncOnValue = identity<In,V>) : 
+            taskFuncOnKey(taskFuncOnKey),
+            taskFuncOnValue(taskFuncOnValue)
+        {}
 
         GroupByKeyOperator(const GroupByKeyOperator& other) : 
-            taskFuncOnKey(other.taskFuncOnKey), taskFuncOnValue(other.taskFuncOnValue), mapContainer(other.mapContainer) {}
+            taskFuncOnKey(other.taskFuncOnKey),
+            taskFuncOnValue(other.taskFuncOnValue),
+            mapContainer(other.mapContainer)
+        {}
 
-        GroupByKeyOperator(GroupByKeyOperator&& other) noexcept: 
-            taskFuncOnKey(std::move(other.taskFuncOnKey)), taskFuncOnValue(std::move(other.taskFuncOnValue)), mapContainer(std::move(other.mapContainer)) {}
+        GroupByKeyOperator(GroupByKeyOperator&& other) noexcept : 
+            taskFuncOnKey(std::move(other.taskFuncOnKey)),
+            taskFuncOnValue(std::move(other.taskFuncOnValue)),
+            mapContainer(std::move(other.mapContainer))
+        {}
+
+        ~GroupByKeyOperator() {}
 
         GroupByKeyOperator& operator+= (const GroupByKeyOperator& other) {
             for (auto otherIt = other.mapContainer.begin(); otherIt != other.mapContainer.end(); otherIt++) {
-                mapIt = mapContainer.find(otherIt->first);
+                auto mapIt = mapContainer.find(otherIt->first);
                 
                 if (mapIt != mapContainer.end()) {
-                    (mapIt->second).insert((mapIt->second).end(), (otherIt->second).begin(), (otherIt->second).end());
+                    mapIt->second.insert(mapIt->second.end(), otherIt->second.begin(), otherIt->second.end());
                 } else {
                     mapContainer[otherIt->first] = otherIt->second;
                 }
             }
-
             return *this ;
         }
 
-        ~GroupByKeyOperator() {
-            delete key;
-            delete val;
-            key = NULL;
-            val = NULL;
-        };
-
         void* svc(void* task) {
-            key = taskFuncOnKey((In*)task);
-            val = taskFuncOnValue((In*)task);
+            K* key = taskFuncOnKey((In*)task);
+            V* val = taskFuncOnValue((In*)task);
             mapContainer[*key].push_back(*val);
 
             return GO_ON;
         }
 
-        MapContainer value(){
+        MapContainer value() {
             return mapContainer;
         }
 
@@ -60,11 +62,7 @@ namespace PpFf {
         std::function< K*(In*) > const& taskFuncOnKey;
         std::function< V*(In*) > const& taskFuncOnValue;
         MapContainer mapContainer;
-        K *key;
-        V *val;
-        typename MapContainer::iterator mapIt;
     };
-
 }
 
 #endif
--- src/operators/ReduceByKeyOperator.hpp
+++ src/operators/ReduceByKeyOperator.hpp
@@ -9,15 +9,26 @@
 namespace PpFf {
 
     template < typename In, typename K, typename V, typename MapContainer >
-    class ReduceByKeyOperator: public FinalOperator {
+    class ReduceByKeyOperator : public FinalOperator {
     public:
         typedef MapContainer Value;
 
-        ReduceByKeyOperator(std::function<K*(In*)> const& taskFuncOnKey, Reducer<In, V> const& reducer): taskFuncOnKey(taskFuncOnKey), reducer(reducer) {}
+        ReduceByKeyOperator(std::function<K*(In*)> const& taskFuncOnKey, Reducer<In, V> const& reducer) :
+            taskFuncOnKey(taskFuncOnKey),
+            reducer(reducer)
+        {}
 
-        ReduceByKeyOperator(const ReduceByKeyOperator& other): taskFuncOnKey(other.taskFuncOnKey), reducer(other.reducer) {}
+        ReduceByKeyOperator(const ReduceByKeyOperator& other) :
+            taskFuncOnKey(other.taskFuncOnKey),
+            reducer(other.reducer)
+        {}
 
-        ReduceByKeyOperator(ReduceByKeyOperator&& other) noexcept: taskFuncOnKey(std::move(other.taskFuncOnKey)), reducer(std::move(other.reducer)) {}
+        ReduceByKeyOperator(ReduceByKeyOperator&& other) noexcept :
+            taskFuncOnKey(std::move(other.taskFuncOnKey)),
+            reducer(std::move(other.reducer))
+        {}
+
+        ~ReduceByKeyOperator() {}
 
         ReduceByKeyOperator& operator+= (ReduceByKeyOperator& other) {
             for (auto otherIt = other.mapContainer.begin(); otherIt != other.mapContainer.end(); otherIt++) {
@@ -34,11 +45,10 @@ namespace PpFf {
             return *this;
         }
 
-        ~ReduceByKeyOperator() {}
-
         void* svc(void* task) {
             K* key = taskFuncOnKey((In*)task);
             auto mapIt = mapContainer.find(*key);
+
             if (mapIt != mapContainer.end()) {
                 mapIt->second = reducer.accumulator(mapIt->second, *((In*)task));
             } else {
