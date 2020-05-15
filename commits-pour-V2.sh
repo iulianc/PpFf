d10d04aef0a95418adedd5e60b0b6628c7ee4011
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-15 12:05:11 -0400

    Modifie script pour connaitre mes commits

--- commits-pour-V2.sh
+++ commits-pour-V2.sh
@@ -1,6 +1,12 @@
 #!
 #git log --author=iulian --pretty=format:%s V1..
-git shortlog --author=iulian V1..
+
+if [[ $# == 0 ]]; then
+    USER='iulian'
+else
+    USER="$1"
+fi
+git shortlog --author=${USER} V1..
 
 
 
