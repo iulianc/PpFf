b766cb5269da65806b207d125dc1acb87a5ca687
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-24 09:39:56 -0400

    Definit bons parametres pour experiences avec WC

--- benchmarks/WC/WC-bm-config.rb
+++ benchmarks/WC/WC-bm-config.rb
@@ -171,8 +171,8 @@ Experience.define( 6,
                 )
 
 # Experiences avec intervalle de confiance
-NB_REPETITIONS_IC = 2 #40
-DONNEES_IC = peu_de_donnees # beaucoup_de_donnees
+NB_REPETITIONS_IC = 40
+DONNEES_IC = beaucoup_de_donnees
 
 Experience.define( 1001,
                    machines: ['java'],
