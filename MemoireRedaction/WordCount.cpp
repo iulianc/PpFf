ccda6a964528e796154a14a5ae8cca6e3caa0964
iulian <iulian.ciubanu@difuze.com>
2020-05-24 22:54:54 -0400

    Ajoute la description de l'application WordCount

--- MemoireRedaction/WordCount.cpp
+++ MemoireRedaction/WordCount.cpp
@@ -19,122 +19,48 @@
 
 using namespace PpFf;
 
-#define DEFAULT_NB_ITERATIONS 5
-#define DEFAULT_INPUT_FILE 
-#define DEFAULT_OUTPUT_FILE_RESULT 
-#define DEFAULT_NB_THREADS 1
-#define DEFAULT_NB_WORDS 78792
+#define DEFAULT_INPUT_FILE "testdata/78792Words.txt"
+#define DEFAULT_FARM_PARALLELISM 1
+
 typedef std::vector<std::string> Words;
 
-Words* splitInWords(std::string* line) {
-    std::string delimiter = " ";
-
-    Words* words = new Words();
-    size_t start = 0, end = 0;
-    do {
-        end = line->find(delimiter, start);
-        size_t len = end - start;
-        words->push_back( line->substr(start, len) );
-        start += len + delimiter.length();
-    } while (end != std::string::npos);
-
-    return words;
-}
-
-std::string* toLowercaseLetters(std::string* data) {
-    std::string* result = new std::string;
-    for (char c: *data) {
-        if ('a' <= c && c <= 'z') {
-            result->push_back(c);
-        } else if ('A' <= c && c <= 'Z') {
-            result->push_back(c-('Z'-'z'));
-        }
-    }
-    return result;
-}
-
-bool notEmpty(std::string* s) {
-    return s->size() > 0;
-}
+#include "auxiliary-functions.hpp"
 
 int main(int argc, char* argv[]) {
-    uint32_t nbIterations = DEFAULT_NB_ITERATIONS;
-    std::string inputFile = DEFAULT_INPUT_FILE;
-	 std::string outputFileResult = DEFAULT_OUTPUT_FILE_RESULT;
-	 uint32_t nbThreads = DEFAULT_NB_THREADS;
-	 uint32_t nbWords = DEFAULT_NB_WORDS;
-	 uint32_t outWords = 0;
+    uint32_t farmParallelism = argc >= 2 ? atoi(argv[1]) : DEFAULT_FARM_PARALLELISM;
+    std::string inputFile = argc >= 3 ? argv[2] : DEFAULT_INPUT_FILE;
 
-    if (argc >= 2) {
-        inputFile = argv[1];
-    }
-
-    if (argc >= 3) {
-        nbIterations = atoi(argv[2]);
-    }
-
-    if (argc >= 4) {
-        nbThreads = atoi(argv[3]);
-    }
-
-    if (argc >= 5) {
-        outputFileResult = argv[4];
-    }
-
-    if (argc >= 6) {
-        nbWords = atoi(argv[5]);
-    }
+    // Utilise pour verifier le bon fonctionnement du programme
+    bool emitOutput = argc >= 4 && atoi(argv[3]) == 1;
+    
+    auto begin = std::chrono::high_resolution_clock::now();
 
     Reducer<std::string, int> reducer(0, 
                                       [](int count, std::string _) { return count + 1; },
                                       std::plus<int>{} );
-    
-    auto begin = std::chrono::high_resolution_clock::now();
 
-    std::unordered_map<std::string, int> currentResult;
-    for (uint32_t i = 0; i < nbIterations; ++i) {
-        currentResult = 
-            Flow
-            ::source(inputFile)
-			.parallel(nbThreads)
-            .flatMap<std::string, std::string, Words>(splitInWords)			
-            .map<std::string, std::string>(toLowercaseLetters)			
-            .find<std::string>(notEmpty)	
-            .reduceByKey<std::string, std::string, int>(reducer);
+    std::unordered_map<std::string, int> currentResult = 
+        Flow
+        ::source(inputFile)
+        .parallel(farmParallelism)
+        .flatMap<std::string, std::string, Words>(splitInWords)			
+        .map<std::string, std::string>(toLowercaseLetters)			
+        .find<std::string>(notEmpty)	
+        .reduceByKey<std::string, std::string, int>(reducer);  
 
-    }
     auto end = std::chrono::high_resolution_clock::now();
-    long duration_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
+    long duration_ms = 
+        std::chrono::duration_cast<std::chrono::milliseconds>(end-begin).count();
 
-    for (auto it = currentResult.begin(); it != currentResult.end(); it++) {
-		  outWords += it->second;
-    }
-
-    fprintf( stderr, "Temps C++  (%3d it.; %2d thr.; %7d words): %5ld ms {%5ld ms/it. } {%7ld w/s. }\n",
-             nbIterations, nbThreads, nbWords, duration_ms, duration_ms / nbIterations, (outWords / (duration_ms / nbIterations)) * 1000 );
-
-	 std::ofstream ofs;
-
-  	 ofs.open (outputFileResult, std::ofstream::out | std::ofstream::app);
-  	 
-	 char buffer[100];
-	 snprintf(buffer, sizeof(buffer), "Temps C++  (%3d it.; %2d thr.; %7d words): %5ld ms {%5ld ms/it. }\n", 
-												nbIterations, nbThreads, nbWords, duration_ms, duration_ms / nbIterations);
-	 ofs << buffer;
-	 ofs.close();
-
-    for (auto it = currentResult.begin(); it != currentResult.end(); it++) {
-        std::string currentResultKey = it->first;
-        int currentResultValue = it->second;
-        std::cout << currentResultKey << " => " << currentResultValue << std::endl;
-    }
+    if (!emitOutput) {
+        printf("%5ld ", duration_ms);
+    } else {
+        for (auto it = currentResult.begin(); it != currentResult.end(); it++) {
+            std::string currentResultKey = it->first;
+            int currentResultValue = it->second;
+            std::cout << currentResultKey << " => " << currentResultValue << std::endl;
+        }
+    } 
 
     return 0;
 }
-
-
-
-
-
-
-
--- MemoireRedaction/WordCount.java
+++ MemoireRedaction/WordCount.java
@@ -6,6 +6,7 @@ import java.nio.file.Files;
 import java.text.DecimalFormat;
 import java.util.AbstractMap.SimpleEntry;
 import java.util.Arrays;
+import java.util.Set;
 import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
@@ -19,78 +20,97 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.*;
+import java.util.HashMap;
+import java.util.function.*;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.toMap;
 
 
-public class WordCount {
-  static final int DEFAULT_NB_ITERATIONS = 5;
-  static final String DEFAULT_INPUT_FILE = "testdata/Words.txt";
-  static final String DEFAULT_OUTPUT_FILE_RESULT = "testdata/wordcount_benchmark_result.txt";
-  static final int DEFAULT_NB_WORDS = 78792;
-  
-  public static void main(String[] args) throws IOException {
-    int nbIterations = DEFAULT_NB_ITERATIONS;
-    String inputFile = DEFAULT_INPUT_FILE;
-	 String outputFileResult = DEFAULT_OUTPUT_FILE_RESULT;
-	 int nbWords = DEFAULT_NB_WORDS;
-	 int outWords = 0;
+/**
+ *
+ * @author iciobanu & Guy Tremblay
+ */
 
-    if (args.length >= 1) {
-      inputFile = args[0];
-    }
-
-    if (args.length >= 2) {
-      nbIterations = Integer.parseInt(args[1]);
-    }
-
-    if (args.length >= 3) {
-       outputFileResult = args[2];
-    }
-
-    if (args.length >= 4) {
-       nbWords = Integer.parseInt(args[3]);
-    }
-
-    long startTime = System.nanoTime();
-        
-    List<Map.Entry<String,Integer>> wordsCount = null;
-    for (int i = 0; i < nbIterations; ++i) {
-      wordsCount = Files.lines(Paths.get(inputFile))
-        .parallel()
-        .flatMap( line -> Arrays.stream(line.trim().split(" ")) )
-        .map( word -> word.replaceAll("[^a-zA-Z]", "").toLowerCase() )
-        .filter( word -> word.length() > 0 )
-        .map( word -> new SimpleEntry<>(word, 1) )
-        .collect( toMap(e -> e.getKey(), e -> e.getValue(), (v1, v2) -> v1 + v2) )
-        .entrySet().stream()
-        .collect( Collectors.toList() );  
+class GroupByKey implements Consumer<String> {
+    @SuppressWarnings("unchecked")
+        private Map<String,Integer> map = (Map) new HashMap();
+    
+    public void accept( String s ) {
+        map.put(s, map.getOrDefault(s, 0) + 1);
     }
     
-    long duration = (System.nanoTime() - startTime) / nbIterations;
+    public void combine( GroupByKey other ) {
+        for ( Map.Entry<String,Integer> entry : other.map.entrySet() ) {
+            Integer value = map.getOrDefault(entry.getKey(), 0);
+            map.put( entry.getKey(), value + entry.getValue() );
+        } 
+    }
 
-	 for( Map.Entry<String, Integer> mapEntry : wordsCount) {
-		 outWords += mapEntry.getValue();
-	 }
-    
-    double milliseconds = (double) duration / 1000000;
-	 String outputResult = "Temps Java (" + String.format("%3d", nbIterations) + " it.;          " + String.format("%7d", nbWords) + " words):" + String.format("%6.0f", milliseconds) + " ms" + " {" + String.format("%5.0f", milliseconds / nbIterations) + " ms/it. }" + " {" + String.format("%6.0f", (outWords / (milliseconds / nbIterations)) * 1000) + " w/s. }";
-    System.err.println(outputResult); 
-        
-	 // Write the result to file
-    try {
-		BufferedWriter writer = new BufferedWriter(new FileWriter(outputFileResult, true));
-		writer.write(outputResult);
-		writer.write("\n");
-		writer.write("\n");
-		writer.close();
-	 }catch (IOException e) {
-		e.printStackTrace();
-	 }
-
-    wordsCount.forEach( x -> 
-                        System.out.println( x.getKey() + " => " + x.getValue() ) );   
-  }
+    public Map<String,Integer> toMap() {
+        return map;
+    }
 }
 
-                             
+public class WordCount {
+    static final String DEFAULT_INPUT_FILE = "testdata/78792Words.txt";
+  
+    public static Stream<String> splitInWords(String line) {
+        return Arrays.stream(line.trim().split(" "));
+    }
+    
+    public static String toLowerCaseLetters(String word) {
+        return word.replaceAll("[^a-zA-Z]", "").toLowerCase();
+    }
+    
+    public static boolean notEmpty( String word ) {
+        return word.length() > 0;
+    }
+    
+    public static void main(String[] args) throws IOException {
+        boolean avecWarmup = Integer.parseInt(args[0]) != 0;
+        String inputFile = args.length >= 2 ? args[1] : DEFAULT_INPUT_FILE;
+
+        // Utilise pour verifier le bon fonctionnement du programme
+        boolean emitOutput = args.length >= 3 && Integer.parseInt(args[2]) == 1;
+
+		// Code bidon pour rechauffement: on emet le nombre d'elements
+		// produits, pour etre certain que le resultat soit utilise
+		// (pour eviter le dead code elimination?).
+        if (avecWarmup) {
+            int wordsCount_ = 
+                Files.lines(Paths.get(inputFile))
+                .parallel()
+                .flatMap( WordCount::splitInWords )
+                .map( WordCount::toLowerCaseLetters )
+                .filter( WordCount::notEmpty )
+                .collect( Collectors.toList() )
+                .size();
+            System.err.println( wordsCount_ );
+        }
+
+        // Execution du *vrai* programme.
+        long startTime = System.nanoTime();
+        
+        Map<String,Integer> wordsCount = 
+            Files.lines( Paths.get(inputFile) )
+            .parallel()
+            .flatMap( WordCount::splitInWords )
+            .map( WordCount::toLowerCaseLetters )
+            .filter( WordCount::notEmpty )
+            .collect( GroupByKey::new, GroupByKey::accept, GroupByKey::combine )
+            .toMap();
+        
+        long duration = (System.nanoTime() - startTime);
+        double milliseconds = (double) duration / 1000000;
+        
+        // On emet le temps ou le resultat complet, selon le cas.
+        if (!emitOutput) {
+            String outputResult = String.format("%6.0f ", milliseconds);
+            System.out.print(outputResult); 
+        } else {
+            wordsCount.entrySet().forEach( x -> 
+                                System.out.println( x.getKey() + " => " + x.getValue() ) );     
+        }
+    }
+}
