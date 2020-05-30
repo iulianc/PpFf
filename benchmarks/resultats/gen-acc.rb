cb5c73f6aa783ff5ac983db896452b89583bea64
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-30 18:27:19 -0400

    Ajoute resultats et graphes pour version optimisee

--- benchmarks/resultats/gen-acc.rb
+++ benchmarks/resultats/gen-acc.rb
@@ -41,26 +41,22 @@ File.new(FICH, chomp: true).readlines.each do |ligne|
   pgm1 = temps[3..5].map(&:to_f)
   pgm2 = temps[6..8].map(&:to_f)
   pgm3 = temps[9..11].map(&:to_f)
-  pgm4 = temps[12..14].map(&:to_f)
 
   seq_ = Array.new(3)
   pgm1_ = Array.new(3)
   pgm2_ = Array.new(3)
   pgm3_ = Array.new(3)
-  pgm4_ = Array.new(3)
 
   seq_ = accs( seq, seq )
   pgm1_ = accs( pgm1, seq )
   pgm2_ = accs( pgm2, seq )
   pgm3_ = accs( pgm3, seq )
-  pgm4_ = accs( pgm4, seq )
 
   fich_sortie.puts format( "%9d ", taille) +
-    format_acc( seq_ ) +
+    format_acc( seq_ )  +
     format_acc( pgm1_ ) +
     format_acc( pgm2_ ) +
-    format_acc( pgm3_ ) +
-    format_acc( pgm4_ )
+    format_acc( pgm3_ )
 end
 
 fich_sortie.close
