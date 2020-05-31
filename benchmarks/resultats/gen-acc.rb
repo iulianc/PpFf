ce859a9326f2f3b219ff91f8f90f28ecf73d2384
Guy Tremblay <tremblay.guy@uqam.ca>
2020-05-31 06:46:15 -0400

    Modifie script pour generetion des accs pour qu'il soit generale

--- benchmarks/resultats/gen-acc.rb
+++ benchmarks/resultats/gen-acc.rb
@@ -38,25 +38,19 @@ File.new(FICH, chomp: true).readlines.each do |ligne|
   taille, *temps = ligne.split
 
   seq = temps[0..2].map(&:to_f)
-  pgm1 = temps[3..5].map(&:to_f)
-  pgm2 = temps[6..8].map(&:to_f)
-  pgm3 = temps[9..11].map(&:to_f)
-
-  seq_ = Array.new(3)
-  pgm1_ = Array.new(3)
-  pgm2_ = Array.new(3)
-  pgm3_ = Array.new(3)
+  pgms = []
+  temps.shift 3
+  while !temps.empty?
+    pgms << temps[0..2].map(&:to_f)
+    temps.shift 3
+  end
 
   seq_ = accs( seq, seq )
-  pgm1_ = accs( pgm1, seq )
-  pgm2_ = accs( pgm2, seq )
-  pgm3_ = accs( pgm3, seq )
+  pgms_ = pgms.map { |pgm| accs(pgm, seq) }
 
   fich_sortie.puts format( "%9d ", taille) +
     format_acc( seq_ )  +
-    format_acc( pgm1_ ) +
-    format_acc( pgm2_ ) +
-    format_acc( pgm3_ )
+    (pgms_.map { |pgm| format_acc pgm }).join
 end
 
 fich_sortie.close
