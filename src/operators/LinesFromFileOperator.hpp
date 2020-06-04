c55720ab27d498df7c9c7ecbd967e92e08420668
Guy Tremblay <tremblay.guy@uqam.ca>
2020-06-04 08:21:40 -0400

    Definit version LinesFromFile avec bloc de strings

--- src/operators/LinesFromFileOperator.hpp
+++ src/operators/LinesFromFileOperator.hpp
@@ -10,16 +10,18 @@
 using namespace ff;
 
 namespace PpFf {
-
+    
     class LinesFromFileOperator: public BaseOperator {
     public:
-        LinesFromFileOperator(const std::string& path) : path(path) { }
+        LinesFromFileOperator(const std::string& path) : path(path)
+        {}
+        LinesFromFileOperator(const LinesFromFileOperator& other) : path(other.path)
+        {}
+        LinesFromFileOperator(LinesFromFileOperator&& other) noexcept : path(std::move(other.path))
+        {}
 
-        LinesFromFileOperator(const LinesFromFileOperator& other) : path(other.path) { }
-
-        LinesFromFileOperator(LinesFromFileOperator&& other) noexcept : path(std::move(other.path)) { }
-
-        ~LinesFromFileOperator() { }
+        ~LinesFromFileOperator()
+        {}
 
         void* svc(void* task) {
             std::ifstream file(path);
@@ -34,6 +36,42 @@ namespace PpFf {
 
     private:
         const std::string& path;
+    };
+
+    class LinesFromFileOperator_: public BaseOperator {
+    public:
+        LinesFromFileOperator_(const std::string& path, const int blockSize) : path(path), blockSize(blockSize)
+        {}
+        LinesFromFileOperator_(const LinesFromFileOperator_& other) : path(other.path), blockSize(other.blockSize)
+        {}
+        LinesFromFileOperator_(LinesFromFileOperator_&& other) noexcept : path(std::move(other.path)), blockSize(other.blockSize)
+        {}
+
+        ~LinesFromFileOperator_()
+        {}
+
+        void* svc(void* task) {
+            std::ifstream file(path);
+
+            //std::cerr << "** blockSize = " << blockSize << std::endl;
+
+            std::string* strings = new std::string[blockSize];
+
+            int next = 0;
+            while (std::getline(file, strings[next])) {
+                ff_send_out(&strings[next]);
+                next += 1;
+                if (next == blockSize) {
+                    strings = new std::string[blockSize];
+                    next = 0;
+                }
+            }
+            return EOS;
+        }
+
+    private:
+        const std::string& path;
+        int blockSize;
     };
 
 }
--- src/operators/SourceOperator.hpp
+++ src/operators/SourceOperator.hpp
@@ -1,7 +1,6 @@
 #ifndef SOURCEOPERATOR_HPP
 #define SOURCEOPERATOR_HPP
 
-//#include <ff/node.hpp>
 #include <ff/ff.hpp>
 #include <functional>
 #include <operators/BaseOperator.hpp>
@@ -16,13 +15,17 @@ namespace PpFf {
     template< typename T, typename It >
     class SourceOperator< T, It, false >: public BaseOperator {
     public:
-        SourceOperator(It &begin, It &end): begin(begin), end(end) {}
+        SourceOperator(It &begin, It &end): begin(begin), end(end)
+        {}
 
-        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
+        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end)
+        {}
 
-        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
+        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end))
+        {}
 
-        virtual ~SourceOperator() {}
+        virtual ~SourceOperator()
+        {}
 
         void* svc(void* task) {
             for (; begin != end; begin++) {
@@ -41,13 +44,17 @@ namespace PpFf {
     template< typename T, typename It >
     class SourceOperator< T*, It, false >: public BaseOperator {
     public:
-        SourceOperator(It const& begin, It const& end): begin(begin), end(end){}
+        SourceOperator(It const& begin, It const& end): begin(begin), end(end)
+        {}
 
-        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
+        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end)
+        {}
 
-        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
+        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end))
+        {}
 
-        ~SourceOperator() {}
+        ~SourceOperator()
+        {}
 
         void* svc(void* task) {
             for (; begin != end; begin++) {
@@ -61,120 +68,6 @@ namespace PpFf {
         It begin;
         It end;
     };
-
-
-
-
-
-
-
-//
-//    template< typename T, typename It >
-//    class SourceOperator< T, It, true >: public BaseOperator {
-//    public:
-//        SourceOperator(It &_begin, It &_end) {
-//            for (; _begin != _end; _begin++) {
-//                container.push_back(*_begin);
-//            }
-//        }
-//
-//        SourceOperator(const SourceOperator& other): container(other.container) {}
-//
-//        SourceOperator(SourceOperator&& other) noexcept: container(std::move(other.container)) {}
-//
-//        virtual ~SourceOperator() {}
-//
-//        void* svc(void* task) {
-//            for(T &val: container){
-//                this->ff_send_out (&val);
-//            }
-//
-//            return EOS;
-//        }
-//
-//    private:
-//        std::vector<T> container {};
-//    };
-//
-//
-//    template< typename T, typename It >
-//    class SourceOperator< T*, It, true >: public BaseOperator {
-//    public:
-//        SourceOperator(It &_begin, It &_end) {
-//            for (; _begin != _end; _begin++) {
-//                container.push_back(*_begin);
-//            }
-//        }
-//
-//        SourceOperator(const SourceOperator& other): container(other.container) {}
-//
-//        SourceOperator(SourceOperator&& other) noexcept: container(std::move(other.container)) {}
-//
-//        ~SourceOperator() {}
-//
-//        void* svc(void* task) {
-//            for(T* val: container){
-//                this->ff_send_out (val);
-//            }
-//
-//            return EOS;
-//        }
-//
-//    private:
-//        std::vector< T* > container {};
-//    };
-
-
-
-
-    //    template< typename T, typename It >
-    //    class SourceOperator: public BaseOperator {
-    //    public:
-    //        SourceOperator(It const& begin, It const& end): begin(begin), end(end) {}
-    //
-    //        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
-    //
-    //        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
-    //
-    //        virtual ~SourceOperator() {}
-    //
-    //        void* svc(void* task) {
-    //            for (; begin != end; begin++) {
-    //                this->ff_send_out(new T(*begin));
-    //            }
-    //
-    //            return EOS;
-    //        }
-    //
-    //    private:
-    //        It begin;
-    //        It end;
-    //    };
-    //
-    //
-    //    template< typename T, typename It >
-    //    class SourceOperator<T*, It>: public BaseOperator {
-    //    public:
-    //        SourceOperator(It const& begin, It const& end): begin(begin), end(end){}
-    //
-    //        SourceOperator(const SourceOperator& other): begin(other.begin), end(other.end) {}
-    //
-    //        SourceOperator(SourceOperator&& other) noexcept: begin(std::move(other.begin)), end(std::move(other.end)) {}
-    //
-    //        ~SourceOperator() {}
-    //
-    //        void* svc(void* task) {
-    //            for (; begin != end; begin++) {
-    //                this->ff_send_out(&(*begin));
-    //            }
-    //
-    //            return EOS;
-    //        }
-    //
-    //    private:
-    //        It begin;
-    //        It end;
-    //    };
 
 }
 
