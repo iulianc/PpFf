886a246ac0b85f534c0c6ef77cd632c1fd791b44
Guy Tremblay <tremblay.guy@uqam.ca>
2020-08-21 14:19:51 -0400

    Change nom de parametres parce que portait a confusion

--- src_new-version/stages/BaseStage.hpp
+++ src_new-version/stages/BaseStage.hpp
@@ -25,72 +25,72 @@ namespace PpFf {
 
         //count; collect; sum; flat; sort
         template< typename Param1 = NULL_TYPE, typename Param2 = NULL_TYPE, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator());
             }
         }
 
         //map; groupByKey
         template< typename Param1, typename Param2, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, std::function<Param2*(Param1*)> const& taskFunc) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<Param2*(Param1*)> const& taskFunc) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc));
             }
         }
 
         //groupByKey
         template< typename Param1, typename Param2, typename Param3 >
-        void addOperator(int const& no_operators, std::function<Param2*(Param1*)> const& taskFunc1, std::function<Param3*(Param1*)> const& taskFunc2) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<Param2*(Param1*)> const& taskFunc1, std::function<Param3*(Param1*)> const& taskFunc2) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc1, taskFunc2));
             }
         }
 
         //find; anyMach; noneMach
         template< typename Param1, typename Param2, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, std::function<Param2(Param1*)> const& taskFunc) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<Param2(Param1*)> const& taskFunc) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc));
             }
         }
 
         //peek
         template< typename Param1, typename Param2, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, std::function<void(Param1*)> const& taskFunc) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<void(Param1*)> const& taskFunc) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc));
             }
         }
         
         //source
         template< typename Param1, typename Param2, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, Param1 &param1, Param2 &param2) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, Param1 &param1, Param2 &param2) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(param1, param2));
             }
         }
 
         //reduce; linesFromFile; reduceByKey
         template< typename Param1, typename Param2 = NULL_TYPE, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, Param1 const& param1) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, Param1 const& param1) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(param1));
             }
         }
 
         //reduceByKey2
         template< typename Param1, typename Param2, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, std::function<void(Param2*, Param1*)> const& taskFunc1, std::function<void(Param2*, Param2*)> const& taskFunc2) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<void(Param2*, Param1*)> const& taskFunc1, std::function<void(Param2*, Param2*)> const& taskFunc2) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc1, taskFunc2));
             }
         }
 
         //min; max;
         template< typename Param1, typename Param2 = NULL_TYPE, typename Param3 = NULL_TYPE >
-        void addOperator(int const& no_operators, std::function<void(Param1*, Param1*)> taskFunc) {
-            for (int i = 0; i < no_operators; i++) {
+        void addOperator(int const& nb_instances, std::function<void(Param1*, Param1*)> taskFunc) {
+            for (int i = 0; i < nb_instances; i++) {
                 operators.push_back(new TOperator(taskFunc));
             }
         }
